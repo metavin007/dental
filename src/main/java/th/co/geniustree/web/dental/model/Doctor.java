7cb5bec600e6edc9488087008dade5d0a93fccb5
metavin007 <meta.vin@hotmail.com>
2015-07-28 12:38:27 +0700

    save 5

--- src/main/java/th/co/geniustree/web/dental/model/Doctor.java
+++ src/main/java/th/co/geniustree/web/dental/model/Doctor.java
@@ -5,10 +5,269 @@
  */
 package th.co.geniustree.web.dental.model;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  *
  * @author Jasin007
  */
-public class Doctor {
+@Entity
+@Table(name = "DOCTOR")
+@AttributeOverride(name = "stateEmployee", column = @Column(name = "STATA_EMPLOYEE"))
+public class Doctor extends SuperEmployee implements Serializable {
+
+    @Column(name = "CERNO")
+    private String cerNo;
+
+    @Column(name = "CERTYPE")
+    private String cerType;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "PID")
+    private String pid;
+
+    @Column(name = "NAMETH")
+    private String nameTh;
+
+    @Column(name = "NAMEENG")
+    private String nameEng;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "BIRTHDAY")
+    private Date birthDay;
+
+    @Column(name = "SEX")
+    private String sex;
+
+    @Column(name = "BLOOD")
+    private String blood;
+
+    @Column(name = "SOLDIERSTATUS")
+    private String soldierStatus;
+
+    @Column(name = "MARRYSTATUS")
+    private String marryStatus;
+
+    @Column(name = "NATION")
+    private String nation;
+
+    @Column(name = "RACE")
+    private String race;
+
+    @Column(name = "ADDRESSPID")
+    private String addressPid;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "TEL")
+    private String tel;
+
+    @Column(name = "MOBILE")
+    private String mobile;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "STARTWORD")
+    private Date startWork;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "ENDWORD")
+    private Date endWork;
+
+    @Column(name = "WORDSTATUS")
+    private String workStatus;
+
+    private boolean enabled = true;
+
+    public String getCerNo() {
+        return cerNo;
+    }
+
+    public void setCerNo(String cerNo) {
+        this.cerNo = cerNo;
+    }
+
+    public String getCerType() {
+        return cerType;
+    }
+
+    public void setCerType(String cerType) {
+        this.cerType = cerType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getNameTh() {
+        return nameTh;
+    }
+
+    public void setNameTh(String nameTh) {
+        this.nameTh = nameTh;
+    }
+
+    public String getNameEng() {
+        return nameEng;
+    }
+
+    public void setNameEng(String nameEng) {
+        this.nameEng = nameEng;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    public String getSoldierStatus() {
+        return soldierStatus;
+    }
+
+    public void setSoldierStatus(String soldierStatus) {
+        this.soldierStatus = soldierStatus;
+    }
+
+    public String getMarryStatus() {
+        return marryStatus;
+    }
+
+    public void setMarryStatus(String marryStatus) {
+        this.marryStatus = marryStatus;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getAddressPid() {
+        return addressPid;
+    }
+
+    public void setAddressPid(String addressPid) {
+        this.addressPid = addressPid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Date getStartWork() {
+        return startWork;
+    }
+
+    public void setStartWork(Date startWork) {
+        this.startWork = startWork;
+    }
+
+    public Date getEndWork() {
+        return endWork;
+    }
+
+    public void setEndWork(Date endWork) {
+        this.endWork = endWork;
+    }
+
+    public String getWorkStatus() {
+        return workStatus;
+    }
+
+    public void setWorkStatus(String workStatus) {
+        this.workStatus = workStatus;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
     
+    
+
 }
---
+++ src/test/java/th/co/geniustree/web/test/MappingStaffTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.web.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import th.co.geniustree.web.dental.App;
+import th.co.geniustree.web.dental.model.Staff;
+import th.co.geniustree.web.dental.repo.StaffRepo;
+
+/**
+ *
+ * @author Jasin007
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = App.class)
+@Transactional(propagation = Propagation.REQUIRED)
+public class MappingStaffTest {
+    
+    @Autowired
+    StaffRepo staffRepo;
+    
+    @Test
+    public void saveStaff(){
+        Staff staff = new Staff();
+        staff.setNameTh("jasin");
+        staff.setMobile("089111");
+        staffRepo.save(staff);
+    }
+}

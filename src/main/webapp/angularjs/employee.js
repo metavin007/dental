dcb5598eaba7d8a66268f29b8bb27243e0e6e87b
metavin007 <meta.vin@hotmail.com>
2015-07-30 02:07:29 +0700

    save 7

--- src/main/webapp/angularjs/employee.js
+++ src/main/webapp/angularjs/employee.js
@@ -3,35 +3,44 @@ angular.module('Employee', [])
         .controller('EmployeeController', function ($scope, $http) {
             $scope.employees = {};
             $scope.employee = {};
-            
-            
+
+
             loadEmployee();
-            function loadEmployee(){
-                $http.get('/loadstaff').success(function (data){
+            function loadEmployee() {
+                $http.get('/loadstaff').success(function (data) {
                     $scope.employees = data;
-                }).error(function (data,status,header,config){
-                    
+                }).error(function (data, status, header, config) {
+
+                });
+            }
+            ;
+
+            $scope.saveEmployee = function () {
+                $http.post('/saveStaff', $scope.employee).success(function (data) {
+                    alert('saveข้อมูลเรียบร้อย');
+
+                }).error(function (data, status, header, config) {
+                    alert('saveข้อมูลล้มเหลว');
                 });
             };
-            
-            $scope.saveEmployee = function () {
-              $http.post('/saveStaff',$scope.employee).success(function (data){
-                    alert('saveข้อมูลเรียบร้อย');
-                    
-              }).error(function (data,status,header,config){
-                    alert('saveข้อมูลล้มเหลว');
-              });                   
-            };
-            
-            $scope.delectEmployee = function (rowemployee){
-               $http.post('/deleteStaff',rowemployee).success(function (data){
+
+            $scope.delectEmployee = function (rowemployee) {
+                $http.post('/deleteStaff', rowemployee).success(function (data) {
                     loadEmployee();
                     alert('ลบข้อมูลเรียบร้อย');
-               }).error(function (data,status,header,config){
-                   alert('ลบข้อมูลล้มเหลว');
-               });
+                }).error(function (data, status, header, config) {
+                    alert('ลบข้อมูลล้มเหลว');
+                });
             };
-            
+
+            $scope.page = 1;
+            $scope.nextPage = function () {
+                $scope.page = 2;
+            };
+            $scope.backPage = function () {
+                $scope.page = 1;
+            };
+
         });
 
 
